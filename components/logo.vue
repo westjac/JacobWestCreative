0e1b978afbdea34311a4adca10d90bd9329c3f45
Jacob B West <jacob.b.west@mines.sdmt.edu>
2021-01-16 23:43:02 -0700

    Added my logo

---
+++ components/logo.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="JWCLogo center">
+    <img src="/jbw-white.png" alt="Jacob West Creative" width="600em">
+  </div>
+</template>
+
+<style scoped>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,18 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        Jacob West Creative
-      </div>
+
+      <logo />
     </v-col>
   </v-row>
 </template>
 
 <script>
+import Logo from '~/components/logo.vue'
 
 export default {
   components: {
+    Logo
   }
 }
 </script>
