0fdcb2e298b634061c4f06a793bf9aeb8f8c15e6
Jacob B West <jacob.b.west@mines.sdmt.edu>
2021-01-17 00:19:23 -0700

    Added button to photo page

--- components/logo.vue
+++ components/logo.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="JWCLogo center">
+  <div class="JWCLogo">
     <img src="/jbw-white.png" alt="Jacob West Creative" width="600em">
   </div>
 </template>
 
 <style scoped>
+.JWCLogo {
+    display: flex;
+    justify-content: center;
+    padding: 5em;
+}
 </style>
--- pages/photo.vue
+++ pages/photo.vue
@@ -1,25 +1,17 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <logo />
       <div class="text-center">
-        <v-btn
-          to="/photo"
-          class="ma-2"
-          outlined
-          large
-          color="white">Photo</v-btn>
+          Photo.
       </div>
     </v-col>
   </v-row>
 </template>
 
 <script>
-import Logo from '~/components/logo.vue'
 
 export default {
   components: {
-    Logo
   }
 }
 </script>
